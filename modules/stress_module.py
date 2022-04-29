ecce3ee2fb531e400cd2bf72d9108af880d96719
vdecaro <valeriodc95@hotmail.it>
2022-04-29 17:25:52 +0200

    partial fix

--- modules/stress_module.py
+++ modules/stress_module.py
@@ -1,8 +1,9 @@
 import os
 import json
+from ast import literal_eval as make_tuple
 
+import tensorflow as tf
 import tensorflow_addons as tfa
-from tensorflow import keras
 
 from base.node import TEACHINGNode
 from base.communication.packet import DataPacket
@@ -12,36 +13,74 @@ from .base_module import LearningModule
 class StressModule(LearningModule):
 
     def __init__(self):
-        super(self, StressModule).__init__()
+        super(StressModule, self).__init__()
         self._model_path = os.getenv('MODEL_PATH')
-        self._hparams_path = os.environ['HPARAMS_PATH'] if self._model_path is None else None
     
     @TEACHINGNode(produce=True, consume=True)
     def __call__(self, input_fn):
 
         for msg in input_fn:
-            x = msg # TODO: process the message as input of the model
+            x = tf.tensor([msg.body['eda']])
             stress_value = self._model(x)
-            yield DataPacket(topic='stress', body={'value': stress_value})
+            yield DataPacket(
+                topic='prediction.stress.value', 
+                timestamp=msg.timestamp,
+                body={'stress': tf.squeeze(stress_value).numpy()})
 
     def _build(self):
         if self._file_path is not None:
-            self._model = keras.models.load_model(self._model_path)
+            self._model = tf.keras.models.load_model(self._model_path)
         else:
-            config = json.loads(open(self._hparams_path).read())
-            inputs = keras.Input(shape=(config['INPUT_SIZE'], None))
-            for i in range(config['LAYERS']):
+            inputs = tf.keras.Input(shape=(int(os.environ['INPUT_SIZE']),))
+            for i in range(int(os.environ['LAYERS'])):
                 x = tfa.layers.ESN(
-                    units=config['UNITS'],
-                    connectivity=config['CONNECTIVITY'],
-                    leaky=config['LEAKY'],
-                    spectral_radius=config['RHO'],
+                    units=int(os.environ['UNITS']),
+                    connectivity=float(os.environ['CONNECTIVITY']),
+                    leaky=float(os.environ['LEAKY']),
+                    spectral_radius=float(os.environ['RHO']),
+                    kernel_initializer='glorot_uniform',
+                    recurrent_initializer=SparseRecurrentTensor(
+                        int(os.environ['UNITS']),
+                        float(os.environ['LEAKY']),
+                        float(os.environ['RHO'])
+                    ),
                     return_sequences=True,
+                    stateful=True
                 )(inputs if i == 0 else x)
-            outputs = keras.layers.Dense(
-                config['CLASSES'], 
-                activation=('sigmoid' if config['CLASSES'] <= 2 else 'softmax')
+            outputs = tf.keras.layers.Dense(
+                int(os.environ['N_CLASSES']), 
+                activation=('sigmoid' if int(os.environ['N_CLASSES']) <= 2 else 'softmax')
             )(x)
-            self._model = keras.Model(inputs=inputs, outputs=outputs, name="stress_model")
+            self._model = tf.keras.Model(inputs=inputs, outputs=outputs, name="stress_model")
 
         self._model.summary()
+
+
+class SparseRecurrentTensor(tf.keras.initializers.Initializer):
+
+  def __init__(self, M, leaky, spectral_radius, connectivity=10, W=None):
+    self.M = M
+    self.leaky = leaky
+    self.spectral_radius = spectral_radius
+    self.connectivity = connectivity
+    self.W = W
+
+  def __call__(self, shape, dtype=None, **kwargs):
+    if not self.W:
+      initializer = tf.keras.initializers.GlorotUniform()
+      recurrent_weights = initializer(shape, dtype)
+      connectivity_mask = tf.cast(tf.math.less_equal(tf.random.uniform(shape), self.connectivity), dtype)
+      recurrent_weights = tf.math.multiply(recurrent_weights, connectivity_mask)
+      abs_eig_values = tf.abs(tf.linalg.eig(recurrent_weights)[0])
+      scaling_factor = tf.math.divide_no_nan(
+          self.spectral_radius, tf.reduce_max(abs_eig_values)
+      )
+      self.W = tf.multiply(recurrent_weights, scaling_factor)
+    return self.W
+
+  def get_config(self):
+    config = super().get_config().copy()
+    config.update({"M": self.M, "leaky": self.leaky, "spectral_radius": self.spectral_radius,
+        "connectivity": self.connectivity, "W": self.W.numpy()
+    })
+    return config
