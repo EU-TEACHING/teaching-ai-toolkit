3233ef348a2588d6a119a7d3951e7a99f019a2d0
Vincenzo Lomonaco <vincenzo.lomonaco@outlook.com>
2023-06-19 16:15:54 +0100

    working or_module

--- modules/or_module.py
+++ modules/or_module.py
@@ -1,5 +1,5 @@
 from tensorflow import keras
-from keras.applications.efficientnet import decode_predictions
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 import numpy as np
 
 from base.node import TEACHINGNode
@@ -16,14 +16,14 @@ class ORModule(LearningModule):
     @TEACHINGNode(produce=False, consume=True)
     def __call__(self, input_fn):
         for msg in input_fn:
-            np_img = msg.body["img"]
+            np_img = np.asarray(eval(msg.body["img"]), dtype='uint8').reshape(224, 224, 3)
             print("image received!")
 
             img_batch = np.expand_dims(np_img, 0)
-            pred = self.model.predict(img_batch)
+            pred = self.model.predict(preprocess_input(img_batch))
             print("Predictions: ", decode_predictions(pred))
 
     def _build(self):
-        self.model = keras.applications.EfficientNetV2B0(weights='imagenet', include_top=True)
+        self.model = keras.applications.MobileNetV2(weights='imagenet', include_top=True)
         self.model.trainable = False
         self.model.summary()
